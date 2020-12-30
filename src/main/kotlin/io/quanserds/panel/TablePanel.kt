8e6e66b6760b7276038918b18462cb62beba5968
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2020-12-30 10:39:52 -0500

    Improve TablePanel

--- src/main/kotlin/io/quanserds/panel/TablePanel.kt
+++ src/main/kotlin/io/quanserds/panel/TablePanel.kt
@@ -4,9 +4,10 @@ import io.quanserds.fx.*
 import io.quanserds.icon.fontIcon
 import javafx.geometry.Insets
 import javafx.geometry.Pos
-import javafx.scene.control.Button
-import javafx.scene.control.Tooltip
+import javafx.scene.control.*
+import org.kordamp.ikonli.materialdesign2.MaterialDesignF
 import org.kordamp.ikonli.materialdesign2.MaterialDesignI
+import org.kordamp.ikonli.materialdesign2.MaterialDesignR
 import org.kordamp.ikonli.materialdesign2.MaterialDesignT
 
 class TablePanel : ControlPanel {
@@ -34,10 +35,68 @@ class TablePanel : ControlPanel {
         vspace()
 
         add(hbox {
+            spacing = 8.0
+            val g = ToggleGroup()
+            val a = RadioButton("Metal")
+            val b = RadioButton("Plastic")
+            a.toggleGroup = g
+            b.toggleGroup = g
+            a.isSelected = true
+            add(a)
+            add(b)
+        })
+
+        add(hbox {
+            align(Pos.CENTER_LEFT)
+            spacing = 8.0
+            add(label("Alpha"))
+            add(slider {
+                value = 50.0
+                width(120.0)
+            })
+            add(textField {
+                text = "0"
+                width(60.0)
+            })
+        })
+
+        add(hbox {
+            align(Pos.CENTER_LEFT)
+            spacing = 16.0
+            val g = ToggleGroup()
+            val a = RadioButton("")
+            val b = RadioButton("Random")
+            a.toggleGroup = g
+            b.toggleGroup = g
+            a.isSelected = true
+            a.graphic = ColorPicker().apply {
+                this.width(120.0)
+                this.translateX = 8.0
+            }
+            a.contentDisplay = ContentDisplay.GRAPHIC_ONLY
+            add(a)
+            add(b)
+        })
+
+        add(hbox {
             align(Pos.CENTER_RIGHT)
             spacing = 8.0
             add(Button("", fontIcon(MaterialDesignT.TRANSFER_DOWN, 20)))
             hspace()
+            add(Button("", fontIcon(MaterialDesignR.REWIND_5, 20)).apply {
+                tooltip = Tooltip("Turn 5 deg counter-clockwise")
+            })
+            add(Button("", fontIcon(MaterialDesignR.REWIND_30, 20)).apply {
+                tooltip = Tooltip("Turn 30 deg counter-clockwise")
+            })
+
+            add(Button("", fontIcon(MaterialDesignF.FAST_FORWARD_30, 20)).apply {
+                tooltip = Tooltip("Turn 30 deg clockwise")
+            })
+
+            add(Button("", fontIcon(MaterialDesignF.FAST_FORWARD_5, 20)).apply {
+                tooltip = Tooltip("Turn 5 deg clockwise")
+            })
             add(Button("", fontIcon(MaterialDesignI.INFORMATION_OUTLINE, 20)).apply {
                 tooltip = Tooltip("SRV03 Bottle Table Info")
             })
